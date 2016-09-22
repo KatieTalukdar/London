ee9637f4c81fdca6a6b1aa8356ed2c9c9b8297b5
Your Name <you@example.com>
2016-09-22 10:08:44 +0100

    added the London app

---
+++ London/prettyBtn.swift
@@ -0,0 +1,19 @@
+//
+//  prettyBtn.swift
+//  London
+//
+//  Created by Kashfa Talukdar on 24/02/2016.
+//  Copyright © 2016 Kashfa Talukdar. All rights reserved.
+//
+
+import UIKit
+
+class prettyBtn: UIButton {
+
+    override func awakeFromNib() {
+        layer.cornerRadius = 5.0
+        backgroundColor = UIColor(red: 214.0/255.0, green: 241.0/255.0, blue: 255.0/255.0, alpha: 1)
+        setTitleColor(UIColor.blueColor(), forState:.Normal)
+    
+    }
+}
